bcb9f2288fb472e6d651f89d45cdb8566b5c2b47
tabrej03dz <tabrejalam770496@gmail.com>
2026-02-10 10:47:23 +0530

    dlf

--- app/Console/Commands/SendBirthdayWishes.php
+++ app/Console/Commands/SendBirthdayWishes.php
@@ -51,9 +51,9 @@ class SendBirthdayWishes extends Command
                 'message'            => $message,
             ]);
             $url = $r->user->api->wishes_api;
-//            if (!$url){
-//                $url = 'https://webhooks.1automations.com/webhook/694e1e278849903df2fd8ff4';
-//            }
+            //      if (!$url){
+            //       $url = 'https://webhooks.1automations.com/webhook/694e1e278849903df2fd8ff4';
+            //      }
             try {
                 $resp = $sender->sendBirthdayWish($r->phone, $url);
 
