640176c7e4e813058a799184a8e4e0403f0dab8a
tabrej03dz <tabrejalam770496@gmail.com>
2026-05-19 13:00:06 +0530

    reate invoice extra button removed

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -35,11 +35,6 @@
                 + Tax Invoice
             </a>
             @endcan
-
-            <a href="{{ route('invoices.create', 'tax') }}"
-               class="inline-flex items-center justify-center px-3 py-2 rounded-md bg-[#46837d] text-white text-sm font-medium hover:bg-[#46837d]">
-                + Tax Invoice
-            </a>
         </div>
     </div>
 
