2d89674a07fce2acee7a2d162fc5ec9e7da0e15a
tabrej03dz <tabrejalam770496@gmail.com>
2026-07-14 15:51:49 +0530

    sdjsdf

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -158,11 +158,13 @@ class BusinessController extends Controller
             ],
 
             // ✅ Dynamic Business Type
-            'business_type_id' => [
-                'required',
-                'integer',
-                Rule::exists('business_types', 'id'),
-            ],
+            // 'business_type_id' => [
+            //     'required',
+            //     'integer',
+            //     Rule::exists('business_types', 'id'),
+            // ],
+
+             'type' => ['required', 'exists:business_types,id'],
 
             'gstin'   => ['nullable', 'string', 'max:50'],
             'address' => ['nullable', 'string', 'max:1000'],
