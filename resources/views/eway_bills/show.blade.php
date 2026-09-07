de9d2545a7139a24e036fed02f6990005fc0f72f
tabrej03dz <tabrejalam770496@gmail.com>
2026-09-07 13:52:23 +0530

    sdlkjf

--- resources/views/eway_bills/show.blade.php
+++ resources/views/eway_bills/show.blade.php
@@ -58,7 +58,23 @@
                 >
                     Print E-Way Bill
                 </a>
-                
+
+                <a
+                    href="{{ route(
+                        'eway-bills.edit',
+                        $ewayBill->id
+                    ) }}"
+                    class="inline-flex items-center justify-center
+                        rounded-lg border border-gray-300
+                        bg-white px-4 py-2 text-sm
+                        font-semibold text-gray-800
+                        hover:bg-gray-50
+                        dark:border-neutral-700
+                        dark:bg-neutral-900
+                        dark:text-white"
+                >
+                    Edit E-Way Bill
+                </a>
 
             </div>
 
